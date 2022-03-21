1641e24d30b7e8e4c0944de3b61d9fb2c76d1b1c
rawalokes <rawallokesh000@gmail.com>
2022-03-21 21:44:33 +0545

    intial setup with application.prop configured

---
+++ src/main/java/com/info/myassistant/MyAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.info.myassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/info/myassistant/MyAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.info.myassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
