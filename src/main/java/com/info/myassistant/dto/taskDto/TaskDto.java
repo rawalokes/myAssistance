b4d94fa627c4a5ca3c8071c03196edb77bcf7b57
rawalokes <rawallokesh000@gmail.com>
2022-03-22 18:11:21 +0545

    add validation on TaskDto

--- src/main/java/com/info/myassistant/dto/taskDto/TaskDto.java
+++ src/main/java/com/info/myassistant/dto/taskDto/TaskDto.java
@@ -3,6 +3,10 @@ package com.info.myassistant.dto.taskDto;
 import com.info.myassistant.model.task.Task;
 import lombok.*;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 /**
@@ -19,14 +23,20 @@ public class TaskDto {
 
     private Integer taskId;
 
+    @NotEmpty(message = "Name cannot be empty")
+    @Size(min = 3,message = "Invalid name")
+    @Pattern(regexp = "^[a-zA-Z]",message = "Name cannot have special character or number")
     private String name;
 
+    @NotEmpty(message = "Starting time cannot be empty")
     private String startTime;
 
+    @NotEmpty(message = "Deadline cannot be empty")
     private String deadline;
 
     private LocalDate date;
 
+    @Size(min = 5,message = "Minimum five character required")
     private String remarks;
 
     /**
@@ -34,6 +44,7 @@ public class TaskDto {
      * @param task
      */
     public TaskDto(Task task){
+
         this.taskId=task.getTaskId();
         this.name=task.getName();
         this.date=task.getDate();
