03e44cc59fe6148d4214eed2f35b866f562651dd
rawalokes <rawallokesh000@gmail.com>
2022-03-22 15:46:58 +0545

    add task and task dto

---
+++ src/main/java/com/info/myassistant/dto/taskDto/TaskDto.java
@@ -0,0 +1,41 @@
+package com.info.myassistant.dto.taskDto;
+
+import com.info.myassistant.model.task.Task;
+import lombok.*;
+
+import java.time.LocalDate;
+
+/**
+ * @author rawalokes
+ * Date:3/21/22
+ * Time:6:29 PM
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class TaskDto {
+
+    private Integer taskId;
+
+    private String name;
+
+    private String startTime;
+
+    private String deadline;
+
+    private LocalDate date;
+
+    private String remarks;
+
+    public TaskDto(Task task){
+        this.taskId=task.getTaskId();
+        this.name=task.getName();
+        this.date=task.getDate();
+        this.startTime=task.getStartTime();
+        this.deadline=task.getDeadline();
+        this.remarks=task.getRemarks();
+    }
+
+}
